91b53a66e116c0d334a0c24c4c22f31fc1e7aa16
Sir Eric <echeungsk@gmail.com>
2020-03-29 21:15:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(array, occasion){
+  let thankYouCards = [];
+  for (let i = 0; i < array.length; i++){
+    thankYouCards.push(`Thank you, ${array[i]}, for the wonderful ${occasion} gift!`)
+  }
+  return thankYouCards;
+}
+
+function countDown(number){
+  while (number > 0){
+    console.log(number);
+    number--;
+  }
+  console.log(number);
+}
