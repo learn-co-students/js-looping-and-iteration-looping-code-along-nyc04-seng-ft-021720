8f59612ed8bf51de47e676767848a87bca5c0a08
Ryan Seit <ryan@ryanseit.com>
2020-03-31 13:46:50 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards(people, event) {
+    let arr = [];
+    for (let i = 0; i < people.length; i++) {
+        let msg = `Thank you, ${people[i]}, for the wonderful ${event} gift!`
+        arr.push(msg)
+    }
+    return arr;
+}
+
+function countDown(num) {
+    let i = 0;
+    while (i <= num) {
+        console.log(num - i);
+        i++;
+    }
+}
